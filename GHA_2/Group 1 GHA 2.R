c37afe3b54ba3341398e82f59a6ecc37f989d171
mirar61 <91824161+mirar61@users.noreply.github.com>
2021-12-02 22:03:34 +0100

    Add files via upload

---
+++ GHA_2/Group 1 GHA 2.R
@@ -0,0 +1,35 @@
+#######################################################################################
+# AUTHORS
+#######################################################################################
+# Cyril Janak, 16-611-287
+# Jonas Husmann, 16-610-917
+# Niklas Kampe, 16-611-618
+# Robin Scherrer, 18-617-969
+
+# ---- requirements
+library(glmnet)
+library(corrplot)
+library(ggplot2)
+
+load("GHA/student-mat-train.RData")
+load("GHA/student-mat-test.RData")
+
+
+# ---- exercise_1
+(n_obs_train <- nrow(train))
+(n_obs_test <- nrow(test))
+
+
+# ---- exercise_2
+(avg_grade <- mean(train$G3))
+(min_grade <- min(train$G3))
+(max_grade <- max(train$G3))
+
+# ---- exercise_3
+(final_grade_hist <- ggplot(data=train, aes(G3)) + 
+    geom_histogram(breaks=seq(2,20, by=1),
+                   col="red",
+                   fill="black",
+                   alpha = 0.2)+
+    labs(title="Histogram Final Math Grade", x="Final Math Grade", y="Count"))
+
