65a93a87e631033342afb30dbceaa0bd8763e1d7
Niklas Kampe <67834846+nikampe@users.noreply.github.com>
2021-12-02 23:14:44 +0100

    excercise 5&6: done
    excercise 5&6: done

--- GHA_2/Group 1 GHA 2.R
+++ GHA_2/Group 1 GHA 2.R
@@ -10,6 +10,7 @@
 library(glmnet)
 library(corrplot)
 library(ggplot2)
+library(dplyr)
 
 load("GHA/student-mat-train.RData")
 load("GHA/student-mat-test.RData")
@@ -32,4 +33,57 @@ load("GHA/student-mat-test.RData")
                    fill="black",
                    alpha = 0.2)+
     labs(title="Histogram Final Math Grade", x="Final Math Grade", y="Count"))
+
+# ---- exercise_5
+(OLS1 <- lm(G3 ~ . ,
+            data=select(train, G3, Medu, Fedu, studytime, schoolsup, higher)))
+(summary(OLS1))
+
+(OLS2 <- lm(G3 ~ . + .^2, 
+            data=select(train, G3, Medu, Fedu, studytime, schoolsup, higher)))
+(summary(OLS2))
+
+# ---- exercise_6
+(OLS3 <- lm(G3 ~ . , 
+            data=select(train, G3, Medu, Fedu, studytime, schoolsup, higher, Pstatus, famrel, failures, famsup,internet)))
+(summary(OLS3))
+
+(OLS4 <- lm(G3 ~ . + .^2, 
+            data=select(train, G3, Medu, Fedu, studytime, schoolsup, higher, Pstatus, famrel, failures, famsup,internet)))
+(summary(OLS4))
+
+MSE_IS_OLS1 <- mean((train$G3 - OLS1$fitted.values)^2)
+MSE_IS_OLS2 <- mean((train$G3 - OLS2$fitted.values)^2)
+MSE_IS_OLS3 <- mean((train$G3 - OLS3$fitted.values)^2)
+MSE_IS_OLS4 <- mean((train$G3 - OLS4$fitted.values)^2)
+
+fit_OLS1 <- predict(OLS1, newdata = test)
+MSE_OOS_OLS1 <- mean((test$G3 - fit_OLS1)^2)
+
+fit_OLS2 <- predict(OLS2, newdata = test)
+MSE_OOS_OLS2 <- mean((test$G3 - fit_OLS2)^2)
+
+fit_OLS3 <- predict(OLS3, newdata = test)
+MSE_OOS_OLS3 <- mean((test$G3 - fit_OLS3)^2)
+
+fit_OLS4 <- predict(OLS4, newdata = test)
+MSE_OOS_OLS4 <- mean((test$G3 - fit_OLS4)^2)
+
+(MSE_IS <- data.frame(model = c("OLS1_IS", "OLS2_IS", "OLS3_IS", "OLS4_IS"), 
+                      MSE = c(MSE_IS_OLS1, MSE_IS_OLS2, MSE_IS_OLS3, MSE_IS_OLS4)))
+(MSE_OOS <- data.frame(model = c("OLS1_OOS", "OLS2_OOS", "OLS3_OOS", "OLS4_OOS"), 
+                       MSE = c(MSE_OOS_OLS1, MSE_OOS_OLS2, MSE_OOS_OLS3, MSE_OOS_OLS4)))
+
+(ggplot(MSE_IS, aes(model, MSE)) +
+  geom_col(color = "red", fill = 'black', alpha = 0.2) +
+  ggtitle("In-Sample MSEs") + 
+  xlab("") +
+  theme_classic())
+
+(ggplot(MSE_OOS, aes(model, MSE)) +
+  geom_col(color = "red", fill = 'black', alpha = 0.2) +
+  ggtitle("Out-Of-Sample MSEs") + 
+  xlab("") +
+  theme_classic())
+
 
