04bd176d9dd9751250a2a715f5d9b1d1df7ca29e
rjscherrer <robin.j.scherrer@gmail.com>
2021-12-13 13:48:23 +0100

    exercise 3: DONE

--- GHA_3/Group 1 GHA 3.R
+++ GHA_3/Group 1 GHA 3.R
@@ -15,4 +15,17 @@ browser_2006 <- read.csv(file = "GHA/browser_2006.csv")
 browser_new <- read.csv(file = "GHA/browser_new.csv")
 
 
-# ---- exercise_1
+# ---- exercise_3
+lasso.cv <- cv.glmnet(x = as.matrix(browser_2006[!names(browser_2006) %in% 
+                                                    c("id", "spend")]), 
+                      y = browser_2006$spend,
+                      type.measure = "mse", 
+                      family = "gaussian", 
+                      nfolds = 5, 
+                      alpha = 1)
+
+coef.lasso.cv <- coef(lasso.cv, s = "lambda.min")
+(best.lin.pred <- data.frame(name = coef.lasso.cv@Dimnames[[1]][coef.lasso.cv@i + 1], 
+                   coefficient = coef.lasso.cv@x) %>% 
+   .[order(abs(.$coefficient), decreasing = T),] %>%
+   .[1:2,])
