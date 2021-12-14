c7133123007a0ae61e066452c29f48a5908ce937
mirar61 <91824161+mirar61@users.noreply.github.com>
2021-12-14 18:14:17 +0100

    Add files via upload

---
+++ GHA_3/Group 1 GHA 3.R
@@ -0,0 +1,39 @@
+#######################################################################################
+# AUTHORS
+#######################################################################################
+# Cyril Janak, 16-611-287
+# Jonas Husmann, 16-610-917
+# Niklas Kampe, 16-611-618
+# Robin Scherrer, 18-617-969
+
+# ---- requirements
+library(grf)
+library(DiagrammeR)
+library(glmnet)
+
+browser_2006 <- read.csv(file = "/Users/jonashusmann/Desktop/GHA_3/browser_2006.csv")
+browser_new <- read.csv(file = "/Users/jonashusmann/Desktop/GHA_3/browser_new.csv")
+
+# ---- exercise_1
+mean(browser_2006$spend)
+
+
+# ---- exercise_2
+
+row_id_1297 <- browser_2006[browser_2006$id==1297,3:ncol(browser_2006)]
+which.max(row_id_1297)
+
+# ---- exercise_3
+lasso.cv <- cv.glmnet(x = as.matrix(browser_2006[!names(browser_2006) %in% 
+                                                    c("id", "spend")]), 
+                      y = browser_2006$spend,
+                      type.measure = "mse", 
+                      family = "gaussian", 
+                      nfolds = 5, 
+                      alpha = 1)
+
+coef.lasso.cv <- coef(lasso.cv, s = "lambda.min")
+(best.lin.pred <- data.frame(name = coef.lasso.cv@Dimnames[[1]][coef.lasso.cv@i + 1], 
+                   coefficient = coef.lasso.cv@x) %>% 
+   .[order(abs(.$coefficient), decreasing = T),] %>%
+   .[1:2,])
