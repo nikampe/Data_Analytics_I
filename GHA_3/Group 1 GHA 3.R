3066cd15da64b01b4fbdec7f52b00e04a861a257
rjscherrer <robin.j.scherrer@gmail.com>
2021-12-14 20:35:07 +0100

    exercise 3: correction

--- GHA_3/Group 1 GHA 3.R
+++ GHA_3/Group 1 GHA 3.R
@@ -19,25 +19,27 @@ browser_new <- read.csv(file = "GHA/browser_new.csv")
 # ---- exercise_1
 mean(browser_2006$spend)
 
-
 # ---- exercise_2
-
 row_id_1297 <- browser_2006[browser_2006$id==1297,3:ncol(browser_2006)]
 which.max(row_id_1297)
 
-# ---- exercise_3
-lasso.cv <- cv.glmnet(x = as.matrix(browser_2006[!names(browser_2006) %in% c("id", "spend")]), 
+# ---- exercise_3.1
+lasso.cv <- cv.glmnet(x = as.matrix(browser_2006[!names(browser_2006) %in% 
+                                                    c("id", "spend")]), 
                       y = browser_2006$spend,
                       type.measure = "mse", 
                       family = "gaussian", 
                       nfolds = 5, 
                       alpha = 1)
 
+plot(lasso.cv)
+
+# ---- exercise_3.2
 coef.lasso.cv <- coef(lasso.cv, s = "lambda.min")
 (best.lin.pred <- data.frame(name = coef.lasso.cv@Dimnames[[1]][coef.lasso.cv@i + 1], 
-                   coefficient = coef.lasso.cv@x) %>% 
-   .[order(abs(.$coefficient), decreasing = T),] %>%
-   .[1:2,])
+                             coefficient = coef.lasso.cv@x) %>% 
+      .[order(abs(.$coefficient), decreasing = T),] %>%
+      .[1:2,])
 
 # ---- exercise_4
 post_lasso = lm(data = browser_2006, "spend ~ officedepot.com + staples.com")
@@ -56,9 +58,3 @@ predictions = data.frame(cbind(predict_post_lasso,predict_all_variables))
   theme_bw())
 
 (correlations = cor(predictions))
-
-
-
-
-
-
