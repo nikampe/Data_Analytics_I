76a9ddd4e593543935c6fbad92d2cad8142a8734
rjscherrer <robin.j.scherrer@gmail.com>
2021-12-20 23:32:51 +0100

    exercise 2: DON E

--- GHA_4/Group 1 GHA 4.R
+++ GHA_4/Group 1 GHA 4.R
@@ -14,4 +14,19 @@ load("GHA/drugs.RData")
 
 
 # ---- exercise_1
-(m_drug <- nrow(drugs[drugs$Gender=="male" & drugs$Soft_Drug==T,])/nrow(drugs))
+(m_s_drug <- (nrow(drugs[drugs$Gender=="male" & drugs$Soft_Drug==T,]) / 
+   nrow(drugs[drugs$Gender=="male",])) |>
+   {\(x) round(x*100, digits = 2)}() |>
+   paste0("%"))
+
+
+# ---- exercise_2
+m_h_drug <- nrow(drugs[drugs$Gender=="male" & drugs$Hard_Drug==T,]) / 
+   nrow(drugs[drugs$Gender=="male",])
+
+f_h_drug <- nrow(drugs[drugs$Gender=="female" & drugs$Hard_Drug==T,]) / 
+   nrow(drugs[drugs$Gender=="female",])
+
+(diff_h_drug <- (m_h_drug - f_h_drug) |>
+      {\(x) round(x*100, digits = 2)}() |>
+      paste0("%"))
